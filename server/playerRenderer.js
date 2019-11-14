07ee1e0e740db632b9c75e7ec5d7ce1b7fb9fd34
Jan Küster <jkuester@uni-bremen.de>
2019-11-14 11:52:47 +0100

    playerRenderer determine context, responses and send to targetUrl

--- server/playerRenderer.js
+++ server/playerRenderer.js
@@ -14,11 +14,45 @@ export default model => `<!doctype html>
     <div class="h5p-content" data-content-id="${model.contentId}"></div>
     <script>
     H5P.externalDispatcher.on('*', function (event) {  
-      console.log(event)
-      const allInputs = document.querySelectorAll('input');
-      allInputs.forEach(function(inp) {
-        console.log(inp.value) 
-      })
+      const responses = []
+      const params = {}
+      const urlParams = new URLSearchParams(window.location.search)
+      const contentId = urlParams.get('contentId')
+      params.userId = urlParams.get('userId')
+      params.sessionId = urlParams.get('sessionId')
+      params.taskId = urlParams.get('taskId')
+      params.contentId = contentId
+      
+      const cid = 'cid-' + contentId
+      const contentSrc = H5PIntegration.contents[cid]
+      const library = contentSrc.library
+      let contentType
+      
+      console.log(H5PIntegration)
+      
+      if (library.indexOf('Blanks') > -1) {
+        contentType = 'blanks'
+        const allInputs = document.querySelectorAll('input');
+        allInputs.forEach(function(inp) {
+            responses.push(inp.value)
+        })
+      }
+      
+      if (library.indexOf('MultiChoice') > -1) {
+        const jsonContent = JSON.parse(contentSrc.jsonContent)
+        contentType = jsonContent.behaviour.type + 'choice'
+        const allInputs = document.querySelectorAll('.h5p-answer');
+        allInputs.forEach(function(inp) {
+          const dataId = inp.getAttribute('data-id')
+          const checked = inp.getAttribute('aria-checked')
+          responses.push({ dataId, checked })
+        })
+      }
+      
+      params.type = contentType
+      params.responses = responses
+      
+      console.log('send params', params)
     });
 </script>
 </body>
